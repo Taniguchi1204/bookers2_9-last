777463573ece46d4301524bb6caf82f1455b1dbd
EC2 Default User <ec2-user@ip-172-31-38-173.ec2.internal>
2021-07-03 23:21:54 +0000

    create mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,18 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'example.com',
+  user_name: 'tatsuya19951204@gmail.com',
+  password: 'nxviafgrkahrnkhr',
+  authentication: :plain,
+  enable_starttls_auto: true
+}
 
   config.action_mailer.perform_caching = false
 
